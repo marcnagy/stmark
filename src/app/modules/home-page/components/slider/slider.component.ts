88c66c6d71441cfaae9d70768fce3c7ad9c808b9
YoussefShoweter <yoyo10ashraf@gmail.com>
2022-10-24 23:59:08 +0200

    Swiper Added

--- src/app/modules/home-page/components/slider/slider.component.ts
+++ src/app/modules/home-page/components/slider/slider.component.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,ViewEncapsulation } from '@angular/core';
 import { SwiperComponent } from "swiper/angular";
-import SwiperCore, { Pagination } from "swiper";
-SwiperCore.use([Pagination]);
+import SwiperCore, { Keyboard, Pagination, Navigation, Virtual, SwiperOptions } from 'swiper';
+SwiperCore.use([Navigation, Pagination]);
 
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.scss']
+  styleUrls: ['./slider.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+
 })
 export class SliderComponent implements OnInit {
-
+  swiperConfig:any;
   constructor() { }
 
   ngOnInit(): void {
+    this.swiperConfig={
+      centeredSlides:true,
+      clickable:true,
+      pagination: {
+        clickable:true,
+      },
+      navigation: {
+        nextEl: '.right-but',
+        prevEl: '.left-but',
+      },
+    }
   }
 
 }
