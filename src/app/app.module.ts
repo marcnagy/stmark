249d1abd09e63b09c798ae04d14bf480dd239fa8
Marc Nagy <mnaguiyoussef@gmail.com>
2022-10-24 17:21:33 +0200

    added the sign in component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { SwiperModule } from 'swiper/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SliderComponent } from './modules/home-page/components/slider/slider.component';
+import { SignInComponentComponent } from './modules/login/components/sign-in-component/sign-in-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SliderComponent
+    SliderComponent,
+    SignInComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/modules/login/components/sign-in-component/sign-in-component.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SignInComponentComponent } from './sign-in-component.component';
+
+describe('SignInComponentComponent', () => {
+  let component: SignInComponentComponent;
+  let fixture: ComponentFixture<SignInComponentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SignInComponentComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SignInComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
