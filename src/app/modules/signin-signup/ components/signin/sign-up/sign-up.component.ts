be0f035cbbf2a61ef85dc8a3ab41ca12e57ce304
YoussefShoweter <yoyo10ashraf@gmail.com>
2022-10-28 14:38:39 +0200

    added signup model

--- src/app/modules/signin-signup/ components/signin/sign-up/sign-up.component.ts
+++ src/app/modules/signin-signup/ components/signin/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { signupuser } from '../../../models/Signup';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
-
+  User: signupuser = new signupuser;
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.User)
   }
+  GetSignUpdata(nationalid:string,phone:string,first:string,second:string,adress:string,email:string){
+this.User.Address=adress;
+this.User.nationalid=nationalid;
+this.User.Phonenum=phone;
+this.User.firstname=first;
+this.User.secondname=second
+this.User.email=email
+}
 
 }
