f0fad5c1a050b8df6b1d2655f7383e3e47cdb7b7
YoussefShoweter <yoyo10ashraf@gmail.com>
2022-10-30 21:53:19 +0200

    added patterns

--- src/app/modules/home-page/components/header/header.component.ts
+++ src/app/modules/home-page/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SignInComponent } from 'src/app/modules/sign-in/components/sign-in/sign-in.component';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
