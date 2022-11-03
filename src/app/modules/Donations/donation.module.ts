0d9067464c56a5142f6228ef4b6eeee755ba390d
YoussefShoweter <yoyo10ashraf@gmail.com>
2022-11-03 12:01:29 +0200

    created donation module

---
+++ src/app/modules/Donations/donation.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DonationRoutingModule } from './donation-routing.module';
+
+@NgModule({
+    declarations:[
+        
+
+    ],
+    imports:[
+        CommonModule,
+        FormsModule,
+        DonationRoutingModule
+
+
+    ]
+
+})
+export class SigninModule{}
