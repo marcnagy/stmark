323faf2e845ce4277302757d1c954d175f7f3b53
Marc Nagy <mnaguiyoussef@gmail.com>
2022-11-27 00:05:45 +0200

    deploy

--- server.js
+++ server.js
@@ -2,14 +2,12 @@ let express = require('express');
 let app =express();
 
 //set port
-var port = process.env.PORT || 8080
+let port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname+'/dist/stmark'));
 
 //routes
-app.get('/',(req,res)=>{
-    resp.sendFile(__dirname+'/dist/stmark/index.html');
+app.get('/*',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/dist/stmark/index.html'));
 });
-app.listen(port,function(){
-    console.log("we are live");
-})
+app.listen(port)
