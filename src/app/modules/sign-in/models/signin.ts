9ae0c0f01e5feeb024daf6fe5fb3f33988caab82
Marc Nagy <mnaguiyoussef@gmail.com>
2022-12-31 19:57:10 +0200

    before last

--- src/app/modules/sign-in/models/signin.ts
+++ src/app/modules/sign-in/models/signin.ts
@@ -1,13 +1,8 @@
 export class SigninModel{
-    constructor(){
-        this.firstName='';
-        this.id=''
-        this.phonenumber=''
-        this.RemembermeFlag=true
-    }
     firstName:string='';
     id:string='';
     phonenumber:string='';
     RemembermeFlag:boolean=false;
+    isadmin:boolean=false;
    
 }
