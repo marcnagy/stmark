f3d7a52def843dc71bc1ae888f53d771090872f0
YoussefShoweter <yoyo10ashraf@gmail.com>
2023-01-03 11:26:43 +0200

    saved

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ let app =express();
 //set port
 let port = process.env.PORT || 8080;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(express.static(__dirname+'/dist/stmark'));
 
 //routes
@@ -11,3 +16,4 @@ app.get('/*',(req,res)=>{
     res.sendFile(path.join(__dirname+'/dist/stmark/index.html'));
 });
 app.listen(port)
+
