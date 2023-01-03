ff0d96614e3b924881b7ca0a66a02ab10c8debfc
YoussefShoweter <yoyo10ashraf@gmail.com>
2023-01-03 11:28:18 +0200

    removed server

--- server.js
+++ server.js
@@ -4,11 +4,7 @@ let app =express();
 //set port
 let port = process.env.PORT || 8080;
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+
 app.use(express.static(__dirname+'/dist/stmark'));
 
 //routes
